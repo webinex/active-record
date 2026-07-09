12286fa8725651a4ba01fbb1b0dc3a547952f793
Webinex Dev <webinex.dev@gmail.com>
2026-07-09 12:49:18 +0300

    Fix: Bump asky version

--- src/Webinex.ActiveRecord.Abstractions/IActiveRecordRepository.cs
+++ src/Webinex.ActiveRecord.Abstractions/IActiveRecordRepository.cs
@@ -2,24 +2,24 @@
 
 namespace Webinex.ActiveRecord;
 
-public interface IActiveRecordRepository<T>
+public interface IActiveRecordRepository<TType>
 {
-    Task<ListSegment<T>> ListSegmentAsync(Query? query = null, bool includeTotal = true, bool readOnly = false);
+    Task<ListSegment<TType>> ListSegmentAsync(Query? query = null, bool includeTotal = true, bool readOnly = false);
     Task<int> CountAsync(FilterRule? filterRule);
 
-    Task<IReadOnlyCollection<T>> ByKeysAsync<TKey>(IEnumerable<TKey> keys, bool readOnly = false)
+    Task<IReadOnlyCollection<TType>> ByKeysAsync<TKey>(IEnumerable<TKey> keys, bool readOnly = false)
         where TKey : notnull;
 
-    Task<IReadOnlyCollection<T>> AddRangeAsync(IEnumerable<T> entities);
+    Task<IReadOnlyCollection<TType>> AddRangeAsync(IEnumerable<TType> entities);
 
-    Task RemoveRangeAsync(IEnumerable<T> entities);
+    Task RemoveRangeAsync(IEnumerable<TType> entities);
 
     Task<bool> AnyAsync(FilterRule? filterRule = null);
 }
 
 public static class ActiveRecordRepositoryExtensions
 {
-    public static async Task<T?> ByKeyAsync<T>(this IActiveRecordRepository<T> repository, object key)
+    public static async Task<TType?> ByKeyAsync<TType, TKey>(this IActiveRecordRepository<TType> repository, TKey key)
     {
         repository = repository ?? throw new ArgumentNullException(nameof(repository));
         key = key ?? throw new ArgumentNullException(nameof(key));
@@ -27,8 +27,16 @@ public static class ActiveRecordRepositoryExtensions
         return result.FirstOrDefault();
     }
 
-    public static async Task<IReadOnlyCollection<T>> GetAllAsync<T>(
-        this IActiveRecordRepository<T> repository,
+    public static async Task<TType?> ByKeyAsync<TType>(this IActiveRecordRepository<TType> repository, object key)
+    {
+        repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        key = key ?? throw new ArgumentNullException(nameof(key));
+        var result = await repository.ByKeysAsync([key]);
+        return result.FirstOrDefault();
+    }
+
+    public static async Task<IReadOnlyCollection<TType>> GetAllAsync<TType>(
+        this IActiveRecordRepository<TType> repository,
         Query? query = null,
         bool readOnly = false)
     {
@@ -36,7 +44,62 @@ public static class ActiveRecordRepositoryExtensions
         return result.Items;
     }
 
-    public static async Task<T> AddAsync<T>(this IActiveRecordRepository<T> repository, T entity)
+    public static async Task<IReadOnlyCollection<TType>> GetAllAsync<TType>(
+        this IActiveRecordRepository<TType> repository,
+        FilterRule? filterRule = null,
+        IEnumerable<SortRule>? sortRules = null,
+        PagingRule? pagingRule = null,
+        bool readOnly = false)
+    {
+        var query = new Query(filterRule, sortRules?.ToArray(), pagingRule);
+        return await repository.GetAllAsync(query, readOnly);
+    }
+
+    public static async Task<TType?> FirstOrDefaultAsync<TType>(
+        this IActiveRecordRepository<TType> repository,
+        FilterRule? filterRule = null,
+        IEnumerable<SortRule>? sortRules = null,
+        bool readOnly = false)
+    {
+        var query = new Query(filterRule, sortRules?.ToArray(), PagingRule.TakeFirst(1));
+        var result = await repository.GetAllAsync(query, readOnly);
+        return result.FirstOrDefault();
+    }
+
+    public static async Task<TType?> FirstAsync<TType>(
+        this IActiveRecordRepository<TType> repository,
+        FilterRule? filterRule = null,
+        IEnumerable<SortRule>? sortRules = null,
+        bool readOnly = false)
+    {
+        var query = new Query(filterRule, sortRules?.ToArray(), PagingRule.TakeFirst(1));
+        var result = await repository.GetAllAsync(query, readOnly);
+        return result.First();
+    }
+
+    public static async Task<TType?> SingleOrDefaultAsync<TType>(
+        this IActiveRecordRepository<TType> repository,
+        FilterRule? filterRule = null,
+        IEnumerable<SortRule>? sortRules = null,
+        bool readOnly = false)
+    {
+        var query = new Query(filterRule, sortRules?.ToArray(), PagingRule.TakeFirst(1));
+        var result = await repository.GetAllAsync(query, readOnly);
+        return result.SingleOrDefault();
+    }
+
+    public static async Task<TType?> SingleAsync<TType>(
+        this IActiveRecordRepository<TType> repository,
+        FilterRule? filterRule = null,
+        IEnumerable<SortRule>? sortRules = null,
+        bool readOnly = false)
+    {
+        var query = new Query(filterRule, sortRules?.ToArray(), PagingRule.TakeFirst(1));
+        var result = await repository.GetAllAsync(query, readOnly);
+        return result.Single();
+    }
+
+    public static async Task<TType> AddAsync<TType>(this IActiveRecordRepository<TType> repository, TType entity)
     {
         repository = repository ?? throw new ArgumentNullException(nameof(repository));
         entity = entity ?? throw new ArgumentNullException(nameof(entity));
@@ -44,7 +107,7 @@ public static class ActiveRecordRepositoryExtensions
         return result.First();
     }
 
-    public static async Task RemoveAsync<T>(this IActiveRecordRepository<T> repository, T entity)
+    public static async Task RemoveAsync<TType>(this IActiveRecordRepository<TType> repository, TType entity)
     {
         repository = repository ?? throw new ArgumentNullException(nameof(repository));
         entity = entity ?? throw new ArgumentNullException(nameof(entity));
